fb8177a549efbae8ddc37c386926bfc71db18f1e
Dhaval Patel <dhaval@envosue.com>
2020-04-08 15:00:20 -0400

    add vagrant using brew

--- brew.sh
+++ brew.sh
@@ -23,6 +23,8 @@ brew cask install google-chrome
 brew cask install firefox
 brew cask install visual-studio-code
 brew cask install virtualbox
+brew cask install vagrant
+brew cask install vagrant-manager
 brew cask install sourcetree
 brew cask install discord
 brew cask install google-backup-and-sync
