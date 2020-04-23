fb43bef2f3987aa178c1718b7a89da242b12b487
Dhaval Patel <dhaval@envosue.com>
2020-04-22 23:54:52 -0400

    feat: Add VSCode settings file
    Copy default VSCode settings file while setting up system

--- vscode.sh
+++ vscode.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 code --install-extension Shan.code-settings-sync
+
+cp -pr \
+    ${dotfiledir}/settings/VSCode-Settings.json \
+    ${homedir}/Library/Application\ Support/Code/User/settings.json
