668e1be65ae53be669457cfec7ddf8ab2c4c68cd
Dhaval Patel <dhaval@envosue.com>
2020-04-29 01:20:17 -0400

    feat: Add brew cleanup to use node from nvm
    Fix brew doctor error while using nodejs from nvm installation
    Prevent homebrew upgrading node version

--- node.sh
+++ node.sh
@@ -13,3 +13,18 @@ export NVM_DIR="$HOME/.nvm"
   && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 nvm install --lts --no-progress
+
+BREW_NODE_HOME='/usr/local/Cellar/node'
+[[ ! -d "${BREW_NODE_HOME}" ]] && \
+    mkdir $BREW_NODE_HOME
+
+ln -s \
+    $NVM_DIR/versions/node/$(nvm current)/ \
+    $BREW_NODE_HOME
+
+# Overwrite node, npm and npx from linked node in
+# /usr/local/Cellar/node to /usr/local/bin/ homebrew
+brew link --overwrite node
+
+# Prevent Homebrew upgrading node version
+brew pin node
