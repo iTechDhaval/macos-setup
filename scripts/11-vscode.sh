721b78c41631626bc4f3e85102362a92721633f1
Dhaval Patel <dhaval@envosue.com>
2022-09-29 13:17:03 -0400

    :fire: Remove vs code sync extension installation
    Use native vs code setting sync with github account

--- scripts/11-vscode.sh
+++ scripts/11-vscode.sh
@@ -3,8 +3,6 @@
 [ -n "${USER_HOME}" ] || USER_HOME=$HOME
 [ -n "${BASE_DIR}" ] || BASE_DIR=$(dirname $(pwd))
 
-code --install-extension Shan.code-settings-sync
-
 cp -pr \
     ${BASE_DIR}/settings/VSCode-Settings.json \
     ${USER_HOME}/Library/Application\ Support/Code/User/settings.json
