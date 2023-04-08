278fc4ca3fd33f05a982e12d01570f5914f63302
Dhaval Patel <dhaval@envosue.com>
2023-04-08 03:25:27 -0400

    :recycle: Remove VSCode settings
    VSCode settings has been setup to sync with github account

--- scripts/11-vscode.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-[ -n "${USER_HOME}" ] || USER_HOME=$HOME
-[ -n "${BASE_DIR}" ] || BASE_DIR=$(dirname $(pwd))
-
-cp -pr \
-    ${BASE_DIR}/settings/VSCode-Settings.json \
-    ${USER_HOME}/Library/Application\ Support/Code/User/settings.json
